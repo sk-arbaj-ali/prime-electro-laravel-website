942c30e0209bbe07ed29c8b32b54f8e17879ee90
SK ARBAJ ALI <skarbajali.one@outlook.com>
2025-01-16 22:11:30 +0530

    Fix: user is seller or not detection problem fixed.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -52,6 +54,13 @@ class User extends Authenticatable
     public function uniqueIds()
     {
         return ['id'];
+    }
+
+    public function isSeller(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value ? true : false
+        );
     }
 
     public function products(): HasMany
